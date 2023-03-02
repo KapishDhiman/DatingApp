73829d7c3dfa390fbef5429a6e3755d6173014cf
Kapish Dhiman <kdkapishdhiman@gmail.com>
2023-03-02 22:21:41 +0530

    Added routing and bootstrap theme

--- clientApp/src/app/register/register.component.ts
+++ clientApp/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -12,7 +14,8 @@ export class RegisterComponent implements OnInit{
 
   model: any = {}
   
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService, private router:Router,
+    private toastr: ToastrService) {}
 
   ngOnInit(): void {
   }
@@ -21,8 +24,12 @@ export class RegisterComponent implements OnInit{
     this.accountService.register(this.model).subscribe({
       next: () => {
         this.cancel();
+        this.router.navigateByUrl('/members')
       },
-      error: error => console.log(error)
+      error: error => {
+        this.toastr.error(error.error),
+        console.log(error)
+      }
     })
     console.log(this.model);
   }
