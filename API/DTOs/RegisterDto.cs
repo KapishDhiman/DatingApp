e56eaf87f66f5c2db22c38579950f3899b149e55
Kapish Dhiman <kdkapishdhiman@gmail.com>
2023-03-03 15:48:47 +0530

    Error handling

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,4 +1,3 @@
-
 using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
@@ -9,6 +8,7 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
